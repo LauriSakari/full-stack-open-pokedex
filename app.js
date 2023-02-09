c9ba08b1b922aca78a566d0884173ec2c6076d82
Lauri Harjukoski <lauriharjukoski@Lauris-MacBook-Air.local>
2023-02-09 13:28:25 +0200

    11.12 trying again with exit code 1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v11') // change this string to ensure a new version deployed
+  res.send('v12') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
