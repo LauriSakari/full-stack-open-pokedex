293a373d46e0a037b4815f42fc7c47065fe7da15
Lauri Harjukoski <lauriharjukoski@Lauris-MacBook-Air.local>
2023-02-10 18:11:21 +0200

    11.12trying actual script health check

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v14') // change this string to ensure a new version deployed
+  res.send('v15') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+if curl https://empty-field-4404.fly.dev/health | grep ok
+then
+    echo "ok working ok"
+    exit 0
+else
+    echo "ok not ok"
+    exit 1
+fi
 
-exit 0 # exit status 1 means that the script "fails"
+ #exit  exit status 1 means that the script "fails"
